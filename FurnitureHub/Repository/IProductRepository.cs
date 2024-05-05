c6d5f35ac752f8eb4b1287c6be8006c342f37874
ShamsHesham <shamshesham7@gmail.com>
2024-05-05 13:47:40 +0300

    product

---
+++ FurnitureHub/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using FurnitureHub.Models;
+
+namespace FurnitureHub.Repository
+{
+    public interface IProductRepository
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        List<Product> SearchByName(string name);
+        void Insert(Product Obj);
+        void Update(Product Obj);
+        void Delete(int id);
+        void Save();
+    }
+}
