3a26bfb1c061cf036769193d334b91335dbfc579
Amr samy <amrsamy99555@gmail.com>
2024-05-05 18:26:18 +0300

    add new Category2

--- FurnitureHub/Controllers/ProductCategoryController.cs
+++ FurnitureHub/Controllers/ProductCategoryController.cs
@@ -15,11 +15,54 @@ namespace FurnitureHub.Controllers
 
         public IActionResult Index()
         {
-            List<ProductCategory> products = ProductCategoryRepository.GetAll();    
+            List<ProductCategory> categoryList = ProductCategoryRepository.GetAll();    
 
             return View("Index", categoryList);
         }
+        public IActionResult Details(int id)    //get the instructor by id 
+        {
+          ProductCategory category = ProductCategoryRepository.GetById(id);
+            return View("Details", category);
+        }
 
+        [HttpGet]
+        public IActionResult New()   //methof for view the form to add new instructor 
+        {
+
+           
+
+            return View("New");
+        }
+
+
+
+        [HttpPost]
+        public IActionResult SaveNew(ProductCategory category)
+        {
+            if (category.Name != null)
+            {
+                ProductCategoryRepository.Insert(category);
+                ProductCategoryRepository.Save();
+                return RedirectToAction("Index", "ProductCategory");
+            }
+            return View("New", category); // Return the "New" view with the invalid model
+        }
+
+        public IActionResult Search(string searchName)
+        {
+            List<ProductCategory> CategoryList;
+
+            if (!string.IsNullOrEmpty(searchName))
+            {
+                CategoryList = ProductCategoryRepository.SearchByName(searchName);
+            }
+            else
+            {
+                CategoryList = ProductCategoryRepository.GetAll();
+            }
+
+            return View("Index", CategoryList);
+        }
 
     }
 }
