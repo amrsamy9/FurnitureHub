226c850869b81963db9c0fbe6a8674eba3f30470
ShamsHesham <shamshesham7@gmail.com>
2024-05-06 16:41:13 +0300

    product

---
+++ FurnitureHub/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿global using FurnitureHub.Repository;
+global using FurnitureHub.Models;
+global using FurnitureHub.ViewModel;
+global using FurnitureHub.Controllers;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using  System.Diagnostics;
+
---
+++ FurnitureHub/ViewModel/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using FurnitureHub.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FurnitureHub.ViewModel
+{
+    public class ProductViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public string ImageURL { get; set; }
+
+        [Display(Name = " Select a Category")]
+        public int CategoryID { get; set; }
+     
+        public List<ProductCategory>? categoryList { get; set; }
+
+    }
+}
